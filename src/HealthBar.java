03e7375ea20b8b4f0f6c5bfdb0c238241775763d
Tauseef Shahid <tauseefs2020@gmail.com>
2025-10-18 16:06:50 -0500

    Finished HealthBar

--- src/HealthBar.java
+++ src/HealthBar.java
@@ -1,2 +1,34 @@
+import java.awt.*;
+
 public class HealthBar {
+    private int health;
+    private Image redImg;
+    private Rectangle redRec;
+    private Rectangle greenRec;
+    private Image greenImg;
+
+    public HealthBar(int h, Image greenImg, Rectangle redRec, Rectangle greenRec, Image redImg) {
+        health = h;
+        this.greenImg = greenImg;
+        this.redRec = redRec;
+        this.greenRec = greenRec;
+        this.redImg = redImg;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void heal() {
+        health++;
+    }
+
+    public void damage() {
+        health--;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(greenImg, greenRec.x, greenRec.y, greenRec.width, greenRec.height, null);
+        g.drawImage(redImg, redRec.x, redRec.y, redRec.width, redRec.height, null);
+    }
 }
